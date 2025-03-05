52bf3a6cd5223257b9edc2ae1b761c1ec6690d18
mutiu4luv <chidiemmamadu@gmail.com>
2025-03-05 14:33:16 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -108,11 +108,11 @@ const Footer = () => {
               <div className="mt-6">
                 <div className="flex items-center gap-3">
                   <FaLocationArrow />
-                  <p>Noida, Uttar Pradesh</p>
+                  <p>Imo Housing, Back of 3Decken</p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
                   <FaMobileAlt />
-                  <p>+91 123456789</p>
+                  <p>+2347031911306</p>
                 </div>
               </div>
             </div>
